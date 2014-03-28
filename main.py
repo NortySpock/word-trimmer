41e1586d3e781f19eb51abf4f936db9502344b1f
David Norton <david.k.norton@gmail.com>
2014-03-27 20:31:03 -0500

    IT WORKS

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from copy import deepcopy
+import string
 
 master_word_list = []
 word_subset = []
-done = False
-found_smaller_word = False
+done = 0
+found_smaller_word = 0
 smaller_word = ''
 
 f = open('dictionary.txt', 'r')
 for line in f:
-   master_word_list.append(line)
+   master_word_list.append(line.strip())
 f.close()
 
 print(master_word_list)
@@ -19,18 +20,19 @@ current_word = deepcopy(starting_word)
 
 while not done:
    for i in range(len(current_word)):
-      word_subset.append(str(starting_word[:i] + starting_word[i+1:]))
+      word_subset.append(str(current_word[:i] + current_word[i+1:]))
 
       
       
-   found_smaller_word = False
+   found_smaller_word = 0
    for i in word_subset:
-      print ("i:"+i)     
+      print ("i:"+i+":")     
       for j in master_word_list:
-         print ("j:"+j)
-         if i == j:
-            found_smaller_word = True
-            smaller_word = i
+         print ("j:"+j+":")
+         if str(i) == str(j):
+            print('FOUND_SMALLER_WORD!')
+            found_smaller_word = 1
+            smaller_word = str(i)
             print(smaller_word)
             break
       if found_smaller_word:
@@ -39,13 +41,15 @@ while not done:
    word_subset.clear()
    if found_smaller_word:
       print("subword:"+smaller_word)
+      current_word = deepcopy(smaller_word)
+      print ("my_current_word:"+current_word+":")
    elif len(current_word) <= 1:
-      done = True
       print("Success.")
+      break
    else:
-      done = True
-      current_word = smaller_word
+      print("Failed:"+current_word)
       print("FAILURE!")
+      break
 
 
 input("waiting for input to  close...")
