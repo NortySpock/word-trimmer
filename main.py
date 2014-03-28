9eb9f6aa0b9d3c67ad86f3c2151a534d672e4e7c
David Norton <david.k.norton@gmail.com>
2014-03-27 19:20:50 -0500

    rolls through word

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-#testing
+import sys
+
+master_word_list = []
+word_subset = []
+
+f = open('dictionary.txt', 'r')
+for line in f:
+   master_word_list.append(line)
+f.close()
+
+starting_word = "startling"
+
+for i in range(len(starting_word)):
+   # print ("start half:", starting_word[:i])
+   # print ("i         :", starting_word[i]
+   # print ("end   half:", starting_word[i:])
+   print (str(starting_word[:i] + " " + starting_word[i:]))
+   # word_subset.append(str(starting_word[:i] + starting_word[i:]))
+
+input("waiting for input to  close...")
 
