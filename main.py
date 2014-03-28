e8bcef56cdc00e5668e759a4aa96df4ffde60b43
David Norton <david.k.norton@gmail.com>
2014-03-27 20:03:29 -0500

    most of the code implemented, probably just need string comparison

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 import sys
+from copy import deepcopy
 
 master_word_list = []
 word_subset = []
+done = False
+found_smaller_word = False
+smaller_word = ''
 
 f = open('dictionary.txt', 'r')
 for line in f:
    master_word_list.append(line)
 f.close()
 
-starting_word = "startling"
+print(master_word_list)
 
-for i in range(len(starting_word)):
-   # print ("start half:", starting_word[:i])
-   # print ("i         :", starting_word[i]
-   # print ("end   half:", starting_word[i:])
-   print (str(starting_word[:i] + starting_word[i+1:]))
-   # word_subset.append(str(starting_word[:i] + starting_word[i:]))
+starting_word = "startling"
+current_word = deepcopy(starting_word)
+
+while not done:
+   for i in range(len(current_word)):
+      word_subset.append(str(starting_word[:i] + starting_word[i+1:]))
+
+      
+      
+   found_smaller_word = False
+   for i in word_subset:
+      print ("i:"+i)     
+      for j in master_word_list:
+         print ("j:"+j)
+         if i == j:
+            found_smaller_word = True
+            smaller_word = i
+            print(smaller_word)
+            break
+      if found_smaller_word:
+         break
+         
+   word_subset.clear()
+   if found_smaller_word:
+      print("subword:"+smaller_word)
+   elif len(current_word) <= 1:
+      done = True
+      print("Success.")
+   else:
+      done = True
+      current_word = smaller_word
+      print("FAILURE!")
+
 
 input("waiting for input to  close...")
-
