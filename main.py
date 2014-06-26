70c00d95293f5d92ee649d670a91804a21764f9d
David Norton <david.k.norton@gmail.com>
2014-06-26 17:24:27 -0500

    got a better dictionary using SCOWL

--- main.py
+++ main.py
@@ -9,27 +9,15 @@ smaller_word = ''
 list_of_subwords = []
 niners = []
 
-f = open('dictionary.txt', 'r')
+f = open('this_dictionary.txt', 'r')
 for line in f:
-  strip_line = line.strip().lower()
-  if len(strip_line) < 1:
-    pass #don't add
-  elif len(strip_line) == 1:
-    if strip_line in('a','i','o'):
-      my_dictionary.add(strip_line)
-  else:
+  strip_line = line.strip()
+  if strip_line == strip_line.lower() and strip_line.isalpha():
     my_dictionary.add(strip_line)
-
-  if len(strip_line) >= 9:
-    niners.append(strip_line)
+    if len(strip_line) >= 9:
+      niners.append(strip_line)
 f.close()
 
-
-
-#outfile = open('output.txt', 'w')
-
-
-# def find_awesome_word():
 
 def find_subword(word_in, existing_list, dictionary_in):
   word_subset = []
@@ -40,22 +28,19 @@ def find_subword(word_in, existing_list, dictionary_in):
 for nine in niners:
   starting_word = nine
   current_word = deepcopy(starting_word)
-  print "-"
-  print "Original:"+starting_word
+  full_word_list = []
+  full_word_list.append(starting_word)
   done = False
   while not done:
     for i in range(len(current_word)):
       word_subset.append(str(current_word[:i] + current_word[i+1:]))
 
-      
-      
     found_smaller_word = 0
     for i in word_subset: 
       if i in my_dictionary:
-        print('found smaller word: ' + i)
         found_smaller_word = 1
         smaller_word = str(i)
-        list_of_subwords.append(smaller_word)
+        full_word_list.append(smaller_word)
         break
       if found_smaller_word:
         break
@@ -64,15 +49,11 @@ for nine in niners:
     if found_smaller_word:
       current_word = deepcopy(smaller_word)
     elif len(current_word) <= 1:
-      print("Success from word of length:"+str(len(starting_word)))
-      
-      #outfile.write()    
+      print("Success! length:"+str(len(starting_word)))
+      for word in full_word_list:
+        print(word)
+      print("-")
       done = True
     else:
-      print("Failed:"+current_word)
-      list_of_subwords = []
       done = True
 
-#outfile.close()
-
-#input("waiting for input to close...")
